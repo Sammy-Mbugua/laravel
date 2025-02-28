0f08d8043631158f8e9c859b1ef8748a12daeeab
Sammy-Mbugua <msammy542@gmail.com>
2025-02-28 21:19:30 +0300

    meet_eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,40 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => '1',
-                'title' => 'director',
-                'salary' => '80000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'programmer',
-                'salary' => '50000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'staff',
-                'salary' => '20000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(!$job){
-            abort(404);
-        }
-        return $job;
-        
-    }
+    protected $table = 'job_listing';
+    protected $fillable = ['title', 'salary'];
+    
 }
